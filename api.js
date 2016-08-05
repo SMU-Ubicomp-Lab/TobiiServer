0feb9d162f03c8327fa6a709d93818b2a55dd882
Xinyi Ding <dxywill@gmail.com>
2016-08-04 23:09:57 -0500

    Initial commit

---
+++ api.js
@@ -0,0 +1,71 @@
+
+//RESTFUL API
+var express = require('express');
+var app = express();
+
+
+app.post('/api/projects', function(req, res) {
+    res.json({
+    	pr_id:"73z7vtv",
+    	pr_info:{Name:"my new project",
+    			 EagleId:"19"},
+    	pr_created:"2015-04-13T11:10:27+0000",
+    	uri:"/api/projects/73z7vtv"
+    });
+});
+
+app.post('/api/participants', function(req, res) {
+	res.json({
+		pa_id: "ymaagxo",
+		pa_info: {
+			EagleId: "73d",
+			Name: "participants005",
+			Notes: ""
+		},
+		pa_project: "73zyvtv",
+	});
+});
+
+app.post('/api/calibrations', function(req, res) {
+	res.json( {
+		ca_id: "xsfsgsg"
+	})
+});
+
+app.post('/api/calibrations/:id/start', function(req, res) {
+	console.log("Start calibrations " + req.params.id);
+	res.end();
+});
+
+app.post('/api/calibrations/:id/status', function(req, res) {
+	console.log("Check the status of calibrations " + req.params.id);
+	res.json({
+		ca_state: "calibrated"
+	});
+})
+
+app.post('/api/recordings', function(req, res) {
+	res.json({
+		rec_id: "aasgasg"
+	})
+});
+
+app.post('/api/recordings/:id/start', function(req, res) {
+	console.log("Start recording " + req.params.id);
+	res.end();
+});
+
+app.post('/api/recordings/:id/stop', function(req, res) {
+	console.log("Stop recording " + req.params.id);
+	res.end();
+});
+
+var server = app.listen(80, function() {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log("Example app listening at http://%s:%s", host, port);
+
+})
+
---
+++ udp.js
@@ -0,0 +1,25 @@
+var dgram = require('dgram');
+var udpPort = process.env.UDPPORT || 3000;
+
+var server = dgram.createSocket('udp4');
+server.bind(udpPort);
+
+
+server.on('listening', function(){
+  console.log('Server started at ', udpPort);
+});
+
+server.on('message', function(msg, info){
+  var message = msg.toString(); // need to convert to string 
+  // since message is received as buffer 
+  // receive the message and do task
+  console.log(message);
+  response = "{ts: \"123456\", s: \"0\", eye: \"right\", pd: \"4.23\"}";
+  server.send(response, 0, response.length, info.port, info.address);
+});
+
+server.on('error', function(){
+  // handle error
+});
+
+
